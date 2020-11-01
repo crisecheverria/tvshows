5235af67ab922e7dd4a3a5a2be44614d91c1abf0
Cristian Echeverria <cristian.echeverria@brinja.se>
2020-11-01 18:25:51 +0100

    Code refactored

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import AppBar from '@material-ui/core/AppBar';
 import { makeStyles } from '@material-ui/core/styles';
 import TvIcon from '@material-ui/icons/Tv';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import Toolbar from '@material-ui/core/Toolbar';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -12,18 +11,44 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+async function fetchEpisodes({imdbID}) {
+  try {
+    const res = await fetch(`https://www.omdbapi.com/?i=${imdbID}&plot=short&r=json&apikey=a7da74f5`);
+    const data = res.json();
+    return Promise.resolve(data);
+  } catch (error) {
+    return Promise.reject(new Error('Oops!'));
+  }
+}
+
+async function fetchRuntime(data) {
+  return Promise.all(data.map(episode => fetchEpisodes(episode)));
+}
+
+function mostUsedWord(str, Title) {
+  let words = str.match(/\w+/g);
+
+  let result = [];
+  let best = -1; 
+  let lookup = {};
+  for(let i = 0; i < words.length; i++) { 
+    let word = words[i];
+    if (lookup[word] == undefined){
+      lookup[word] = 0;
+    } 
+    lookup[word]++;
+    if(lookup[word] > best) {
+      result = [word];
+      best = lookup[word];
+    } else if (lookup[word] === best) {
+      result.push(word);
+      best = lookup[word];
+    }
+  }
+
+  return {words: result, num: best, title: Title}
 }
 
 function App() {
@@ -31,8 +56,13 @@ function App() {
     title: '',
     season: '',
     avgRaiting: 0,
-    data: []
-  })
+    data: [],
+    runtime: '0',
+    mostFrequentWord: '',
+    episodeTitle: '',
+    error: ''
+  });
+  const [loading, setLoading] = useState(false);
   const classes = useStyles();
 
   function handleChange(event) {
@@ -43,21 +73,99 @@ function App() {
   }
 
   async function fetchTvShow() {
-    const {title, season} = show
-    const tvShows = await fetch(`https://www.omdbapi.com/?t=${title}&Season=${season}&apikey=a7da74f5`);
-    const response = await tvShows.json()
-    const ratings = response.Episodes.map(({ imdbRating }) => imdbRating);
-    const averageRating = ratings.reduce((prev, curr) => {
-        return parseFloat(prev) + parseFloat(curr)
-    }, 0) / ratings.length;
-    setShow((prevState) => ({
-      ...prevState,
-      avgRaiting: averageRating,
-      data: response
-    }))
+    try {
+      setLoading(true)
+      const {title, season} = show;
+      const tvShows = await fetch(`https://www.omdbapi.com/?t=${title}&Season=${season}&apikey=a7da74f5`);
+      const response = await tvShows.json();
+
+      if (response.Response === 'False') {
+        setLoading(false)
+        setShow({
+          ...show,
+          avgRaiting: 0,
+          data: [],
+          runtime: '',
+          error: response.Error,
+          mostFrequentWord: '',
+          episodeTitle: ''
+        })
+        return;
+      }
+
+      const averageRating = response.Episodes.reduce((prev, curr) => {
+        return parseFloat(prev) + parseFloat(curr.imdbRating)
+      }, 0) / response.Episodes.length;
+      const episodesInformation = await fetchRuntime(response.Episodes)
+      const totalRuntime = episodesInformation
+        .reduce((prev, curr) => {
+          return prev + (+curr.Runtime.replace(/\D/g, ""));
+        }, 0);
+
+      const mostUsedWordPerPlot = episodesInformation.map(({Title, Plot}) => mostUsedWord(Plot, Title)).sort((a, b) => (a.num - b.num));
+
+      let mostFrequentWord = [];
+      let best = -1;
+      let lookup = {};
+      let episodeTitle = [];
+      let num = 0;
+
+      for(let i = 0; i < mostUsedWordPerPlot.length; i++) { 
+        let words = mostUsedWordPerPlot[i]['words'];
+        num = mostUsedWordPerPlot[i]['num'];
+        let title = mostUsedWordPerPlot[i]['title'];
+        if (words.length > 1) {
+          for (let w of words) {
+            if (lookup[w] == undefined) {
+              lookup[w] = 0;
+            }
+            lookup[w] += num
+            if(lookup[w] > best) {
+              mostFrequentWord = [w];
+              episodeTitle = [title];
+              best = lookup[w];
+            } else if (lookup[w] === best) {
+              mostFrequentWord.push(w);
+              episodeTitle.push(title);
+              best = lookup[w];
+            }
+          }
+        }
+        
+        if (lookup[words] == undefined) {
+          lookup[words] = 0;
+        }
+        lookup[words] += num
+        if(lookup[words] > best) {
+          mostFrequentWord = [words]
+          episodeTitle = [title]
+          best = lookup[words];
+        } else if (lookup[words] === best) {
+          mostFrequentWord.push(words);
+          episodeTitle.push(title);
+          best = lookup[words];
+        }
+      }
+
+      setShow((prevState) => ({
+        ...prevState,
+        avgRaiting: Math.round((averageRating + Number.EPSILON) * 100) / 100,
+        data: response,
+        runtime: `${totalRuntime} min`,
+        mostFrequentWord,
+        episodeTitle,
+        error: ''
+      }))
+    } catch (error) {
+      console.log(error)
+    }
+    setLoading(false)
   }
 
-  console.log('Show: ', show)
+  function validate(){
+    const {title, season} = show
+    return title !== '' && title.length > 0 && season > 0
+  }
 
   return (
     <div className={classes.root}>
@@ -94,14 +202,49 @@ function App() {
                     value={show.season} 
                     onChange={handleChange} 
                   />
-                  <Button variant="contained" color="primary" onClick={fetchTvShow}>
-                    Search
-                  </Button>
+                  <div className={classes.wrapper}>
+                    <Button
+                      variant="contained"
+                      color="default"
+                      disabled={!validate()}
+                      onClick={fetchTvShow}
+                    >
+                      Search
+                    </Button>
+                    {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+                  </div>
                 </form>
+                {show.error && (
+                    <Typography className={classes.errorMsg}>
+                        {show.error}
+                    </Typography>
+                  )
+                }    
                 <Card className={classes.card}>
                   <CardContent className={classes.cardContent}>
                     <Typography>
-                      Average Rating: {show.avgRaiting}
+                      Average rating:
+                    </Typography>
+                    <Typography className={classes.boldText}>
+                      {show.avgRaiting}
+                    </Typography>
+                    <Typography>
+                      Total runtine of the season in minutes:
+                    </Typography>
+                    <Typography className={classes.boldText}>
+                      {show.runtime ? show.runtime : '0 min'}
+                    </Typography>
+                    <Typography>
+                      Most frequent word in Plot text from the episodes: 
+                    </Typography>
+                    <Typography className={classes.boldText}>
+                      {show.mostFrequentWord ? show.mostFrequentWord : '?'}
+                    </Typography>
+                    <Typography>
+                      Episode Title with most occurrences of the most frequent word: 
+                    </Typography>
+                    <Typography className={classes.boldText}>
+                      {show.episodeTitle ? show.episodeTitle : '?'}
                     </Typography>
                   </CardContent>
                 </Card>
@@ -109,11 +252,6 @@ function App() {
           </Grid>
         </Container>
       </main>
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Copyright />
-        </Container>
-      </footer>
     </div>
   );
 }
@@ -128,17 +266,10 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-around',
-    margin: theme.spacing(10, 0),
+    margin: theme.spacing(5, 0),
   },
   icon: {
     marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
   },
   cardGrid: {
     paddingTop: theme.spacing(8),
@@ -149,18 +280,28 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
   },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
   cardContent: {
     flexGrow: 1,
   },
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
   },
+  buttonProgress: {
+    color: 'green',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+  errorMsg: {
+    padding: theme.spacing(2, 10),
+    color: 'red'
+  },
+  boldText: {
+    fontWeight: 'bold'
+  }
 }));
 
 export default App;
